6efbb239fa1e9a2051983c683fc084228cbc65fb
evoskuil <eric@voskuil.org>
2019-10-26 16:12:05 -0700

    Add buildbase support for VS2017.

--- build/buildbase_static.bat
+++ build/buildbase_static.bat
@@ -5,6 +5,7 @@ SET solution=%1
 SET version=%2
 SET log=build_%version%.log
 SET tools=Microsoft Visual Studio %version%.0\VC\vcvarsall.bat
+IF %version% == 15 SET tools=Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat
 SET environment="%programfiles(x86)%\%tools%"
 IF NOT EXIST %environment% SET environment="%programfiles%\%tools%"
 IF NOT EXIST %environment% GOTO no_tools
@@ -15,7 +16,7 @@ CALL %environment% x86 > nul
 ECHO Platform=x86
 
 ECHO Configuration=StaticDebug
-msbuild /m /v:n /p:Configuration=StaticDebug /p:Platform=Win32 %solution% > %log%
+msbuild /m /v:n /p:Configuration=StaticDebug /p:Platform=Win32 %solution% >> %log%
 IF errorlevel 1 GOTO error
 ECHO Configuration=StaticRelease
 msbuild /m /v:n /p:Configuration=StaticRelease /p:Platform=Win32 %solution% >> %log%
